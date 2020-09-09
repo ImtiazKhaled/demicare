46f8e6dba8819fcfbdc5e07e4b60a04b6e85df5b
tisral <64937810+tisral@users.noreply.github.com>
2020-09-09 00:32:25 -0500

    Modal inserted in Dementia card
    Modal inserted in Dementia card

--- website/src/components/dementia_card.js
+++ website/src/components/dementia_card.js
@@ -1,17 +1,37 @@
-import * as React from 'react';
-import {Card, StyledBody } from 'baseui/card';
-import ReactMD from 'react-markdown'
+import * as React from "react";
+import { Card, StyledBody, StyledAction } from "baseui/card";
+import { Button } from "baseui/button";
+import { Modal, ModalHeader, ModalBody, ModalFooter, ModalButton, SIZE, ROLE } from "baseui/modal";
+import { KIND as ButtonKind } from "baseui/button";
+import ReactMD from "react-markdown";
 
 const DementiaCard = (props) => {
-  return <Card
-    overrides={{Root: {style: {width: '328px'}}}}
-    headerImage={props.image}
-    title={props.title}
-  >
-    <StyledBody>
-      <ReactMD source={props.description} />
-    </StyledBody>
-  </Card>
-}
+  const [isOpen, setIsOpen] = React.useState(false);
 
-export default DementiaCard
+  return (
+    <div>
+      <Card overrides={{ Root: { style: { width: "328px" } } }} headerImage={props.image} title={props.title}>
+        <StyledBody>
+          <ReactMD source={props.description} />
+        </StyledBody>
+
+        <StyledAction>
+          <Button onClick={() => setIsOpen(true)} overrides={{ BaseButton: { style: { width: "100%" } } }}>
+            More
+          </Button>
+        </StyledAction>
+      </Card>
+
+      <Modal onClose={() => setIsOpen(false)} closeable isOpen={isOpen} animate autoFocus size={SIZE.default} role={ROLE.alertdialog}>
+        <ModalHeader>Hello world</ModalHeader>
+        <ModalBody>Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non facilisis nisl. Maecenas aliquet mauris ut tempus.</ModalBody>
+        <ModalFooter>
+          <ModalButton kind={ButtonKind.tertiary}>Cancel</ModalButton>
+          <ModalButton>Okay</ModalButton>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+export default DementiaCard;

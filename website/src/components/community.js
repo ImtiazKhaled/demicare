063f68696c1a5abb6d578aa8fb03704ad05cbca9
imtiaz <imtiazkhaled07@gmail.com>
2020-09-23 21:59:56 -0500

    put everything together

--- website/src/components/community.js
+++ website/src/components/community.js
@@ -6,22 +6,38 @@ import { Button } from "baseui/button";
 import ReactMD from "react-markdown";
 import { t } from "react-switch-lang";
 
-let mdData = `
-### Bilingual in English and Korean \n
-Phone number: [972-245-1573](tel:9722451573) \n
-Address: [1618 Kirby Rd, Carrollton, TX 75006](https://www.google.com/maps/dir/32.6627151,-97.1251708/Carrollton+Health+%26+Rehabilitation+Center+Carrollton,+TX+75006/@32.8051728,-97.1511311,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x864c26479c9a28d9:0x81db9789c4704bfb!2m2!1d-96.8987142!2d32.9473194) \n
-Website: [carrolltonhealth.com](https://carrolltonhealth.com)
-`;
 
-export default () => {
+const Community = () => {
+  
+  const [ markdown, setMarkdown ] = React.useState(`### Bilingual in English and Korean \n
+  Phone number: [972-245-1573](tel:9722451573) \n
+  Address: [1618 Kirby Rd, Carrollton, TX 75006](https://www.google.com/maps/dir/0000,0000/Carrollton+Health+%26+Rehabilitation+Center+Carrollton,+TX+75006/@32.8051728,-97.1511311,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x864c26479c9a28d9:0x81db9789c4704bfb!2m2!1d-96.8987142!2d32.9473194) \n
+  Website: [carrolltonhealth.com](https://carrolltonhealth.com)
+  `)
+
+  const getLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( (position) => {
+        var tempMarkdown = markdown.replace('0000',position.coords.latitude.toString()) 
+        tempMarkdown = tempMarkdown.replace('0000',position.coords.longitude.toString()) 
+        setMarkdown(tempMarkdown)
+      });            
+    } else {
+      console.error('This browser does not support locations')
+    }
+  }
+  
+  React.useEffect(() => {
+    getLocation()
+  })
 
   const showAllLocations = () => {
     console.log('should show all maps now')
   }
 
   const DATA = [
-    ["Carrolton Health & Rehabilitation Center", mdData],
-    ["Carrolton Health & Rehabilitation Center", mdData],
+    ["Carrolton Health & Rehabilitation Center", markdown],
+    ["Carrolton Health & Rehabilitation Center", markdown],
   ];
 
   return (
@@ -51,3 +67,5 @@ export default () => {
     </div>
   );
 };
+
+export default Community

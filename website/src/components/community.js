4eed497641fc2c1f986ae73286213581dbec8a7a
tisral <64937810+tisral@users.noreply.github.com>
2020-10-02 03:47:13 -0500

    Cloud Firestore Database Integration
    Cloud Firestore Database Integration

--- website/src/components/community.js
+++ website/src/components/community.js
@@ -1,40 +1,37 @@
-import * as React from 'react'
-import { StyledTable, StyledHead, StyledHeadCell, StyledBody } from 'baseui/table'
-import { Display2 } from 'baseui/typography'
-import { Button } from 'baseui/button'
-import { t, getLanguage } from 'react-switch-lang'
-import CommunityRow from './community_row'
-import DATA from './common/community.json'
+import React, { useEffect, useState } from "react";
+import { StyledTable, StyledHead, StyledHeadCell, StyledBody } from "baseui/table";
+import { Display2 } from "baseui/typography";
+import { Button } from "baseui/button";
+import { t, getLanguage } from "react-switch-lang";
+import CommunityRow from "./community_row";
+import db from "./common/Firebase";
 
 const Community = (props) => {
+  const [DATA, setResources] = useState([]);
+  useEffect(() => {
+    db.collection("facilities").onSnapshot((snapshot) => setResources(snapshot.docs.map((doc) => doc.data())));
+  }, []);
 
-  const [ resources, setResources ] = React.useState(DATA)
+  let resources;
 
-  const showAllLocations = () => {
-    console.log('should show all maps now')
+  if (getLanguage() === "en") {
+    resources = DATA;
+  } else {
+    resources = DATA.filter((facility) => facility.lang === getLanguage());
   }
 
-  React.useEffect(() => {
-    switch(getLanguage()) {
-      case 'ko':
-        setResources(DATA.filter( row => row.lang === 'korean'))
-        break
-      case 'zh':
-        setResources(DATA.filter( row => row.lang === 'chinese'))
-        break
-      default:
-        break
-    }
-  },[props.lang])
-  
+  const showAllLocations = () => {
+    console.log("should show all maps now");
+  };
+
   return (
-    <div className='community-container'>
-      <Display2 marginBottom='scale1000'> {t('communityResources')} </Display2>
-      <Button onClick={() => showAllLocations()}> {t('seeFacilities')} </Button> <br /> <br />
+    <div className="community-container">
+      <Display2 marginBottom="scale1000"> {t("communityResources")} </Display2>
+      <Button onClick={() => showAllLocations()}> {t("seeFacilities")} </Button> <br /> <br />
       <StyledTable>
         <StyledHead>
-          <StyledHeadCell>{t('title')}</StyledHeadCell>
-          <StyledHeadCell>{t('resource')}</StyledHeadCell>
+          <StyledHeadCell>{t("title")}</StyledHeadCell>
+          <StyledHeadCell>{t("resource")}</StyledHeadCell>
         </StyledHead>
         <StyledBody>
           {resources.map((row, index) => (
@@ -42,9 +39,9 @@ const Community = (props) => {
           ))}
         </StyledBody>
       </StyledTable>
-      <div style={{margin: '10vh'}} />
+      <div style={{ margin: "10vh" }} />
     </div>
-  )
-}
+  );
+};
 
-export default Community
+export default Community;
--- website/src/components/dementia.js
+++ website/src/components/dementia.js
@@ -1,34 +1,16 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import DementiaCard from "./dementia_card";
 import { Display2 } from "baseui/typography";
 import { FlexGrid, FlexGridItem } from "baseui/flex-grid";
 import { t } from "react-switch-lang";
-
-let dementia = [
-  {
-    id: "1",
-    title: "10 Signs of Alzheimer's",
-    description: "Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non facilisis nisl.",
-    image: "https://source.unsplash.com/user/erondu/700x400",
-    video: "https://www.youtube.com/embed/QmLStxU12F0",
-  },
-  {
-    id: "2",
-    title: "Interactive Brain Tour",
-    description: "Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non facilisis nisl.",
-    image: "https://source.unsplash.com/user/erondu/700x400",
-    video: "https://www.youtube.com/embed/9DLw3cCfbm0",
-  },
-  {
-    id: "3",
-    title: "Caregivers",
-    description: "Proin ut dui sed metus pharetra hend rerit vel non mi. Nulla ornare faucibus ex, non facilisis nisl.",
-    image: "https://source.unsplash.com/user/erondu/700x400",
-    video: "https://www.youtube.com/embed/cRLB7WqX0fU",
-  },
-];
+import db from "./common/Firebase";
 
 const Dementia = () => {
+  const [dementia, setResources] = useState([]);
+  useEffect(() => {
+    db.collection("dementia-info").onSnapshot((snapshot) => setResources(snapshot.docs.map((doc) => doc.data())));
+  }, []);
+
   return (
     <div className="dementia-container">
       <Display2 marginBottom="scale1000"> {t("dementiaInformation")} </Display2>

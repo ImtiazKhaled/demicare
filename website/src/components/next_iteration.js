2ea37a796786bea7b2501914ae96623609f948f2
Hoang Luu <hoang.luu8@mavs.uta.edu>
2020-10-19 23:41:12 -0500

    added next iteration timeline

--- website/src/components/next_iteration.js
+++ website/src/components/next_iteration.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import picture from './future.png'
-
+import timeline from './timeline.png'
 const NextIter = () => {
 
     return(
         <div style={{alignContent:'space-around'}}>
             <img src={picture} height='auto' width='100%'></img>
+            <img src={timeline} height='auto' width='100%'></img>
         </div>
         
     )

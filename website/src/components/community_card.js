7a5fde65306fc33d1bf0c1adfec665ec585ce880
tisral <64937810+tisral@users.noreply.github.com>
2020-11-08 00:35:08 -0600

    Update community_card.js

--- website/src/components/community_card.js
+++ website/src/components/community_card.js
@@ -4,17 +4,25 @@ import { StyledBodyCell } from "baseui/table-grid"
 import { Card } from "baseui/card"
 import ReactMD from "react-markdown"
 
+import { StyledLink } from "baseui/link";
+
 
 const CommunityRow = (props) => {
-  const [markdown, setMarkdown] = React.useState(props.description)
+  // const [markdown, setMarkdown] = React.useState(props.description)
+  const [address, setAddress] = React.useState(props.gmaps);
+  const tel = `tel:${props.phoneNumber}`;
 
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        var tempMarkdown = markdown.replace("0000", position.coords.latitude.toString())
-        tempMarkdown = tempMarkdown.replace("0000", position.coords.longitude.toString())
-        tempMarkdown = tempMarkdown.replaceAll("<newline>", `\n\n`)
-        setMarkdown(tempMarkdown)
+        // var tempMarkdown = markdown.replace("0000", position.coords.latitude.toString())
+        // tempMarkdown = tempMarkdown.replace("0000", position.coords.longitude.toString())
+        // tempMarkdown = tempMarkdown.replaceAll("<newline>", `\n\n`)
+        // setMarkdown(tempMarkdown)
+
+        let tempAddress = address.replace("0000", position.coords.latitude.toString());
+        tempAddress = tempAddress.replace("0000", position.coords.longitude.toString());
+        setAddress(tempAddress);
       })
     } else {
       console.error("This browser does not support locations")
@@ -25,6 +33,9 @@ const CommunityRow = (props) => {
     getLocation()
   }, [props.description])
 
+
+
+
   return (
     <Card>
       <StyledRow key={props.id}>
@@ -33,7 +44,21 @@ const CommunityRow = (props) => {
         </StyledCell>
         <StyledCell>
           <StyledBodyCell>
-            <ReactMD source={markdown} />
+            <p> Phone Number :   &nbsp; <a href={tel}>{props.phoneNumber}</a></p>
+
+            <p>Address : &nbsp;
+              <StyledLink href={address}>
+                {props.address}
+              </StyledLink>
+            </p>
+
+
+            <p>Website : &nbsp;
+              <StyledLink href={props.url}>
+                Link to Website
+             </StyledLink>
+            </p>
+
           </StyledBodyCell>
         </StyledCell>
       </StyledRow>
